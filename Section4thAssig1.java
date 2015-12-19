e93c843196a8e198b2e28cbbe0c630c5d39caec4
ravinder singh <ravinder@atjam.com>
2015-12-19 15:36:16 +0530

    Session 4 Part II Assignment:Print the array in reverse order.
    Done

---
+++ Section4thAssig1.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.assignment.example;
+
+import java.util.Scanner;
+
+/**
+ * @author ravinder
+ *
+ */
+public class Section4thAssig1 {
+
+	/**
+	 * 
+	 */
+	public Section4thAssig1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []arrayx =new int[]{10,20,30,40,50};
+		for(int i=arrayx.length-1;i>=0;i--)
+		{
+			System.out.println("At index "+i+": "+arrayx[i]);
+		}
+		int []arrayinput =new int[5];
+		System.out.println("Enter five values :");
+		Scanner sc = new Scanner(System.in);
+		for(int i =0 ;i<=arrayinput.length-1;i++)
+		{
+			arrayinput[i]=sc.nextInt();
+		}
+	    
+		System.out.println("Print values :");
+		for(int i=arrayinput.length-1;i>=0;i--)
+		{
+			System.out.println("At index "+i+": "+arrayinput[i]);
+		}
+		
+		
+		int [][]array2d =new int[][]{{10,20},{20,30},{30,40},{40,50},{50,60}};
+		for(int j=array2d.length-1;j>=0;j--)
+		{
+			for(int i=0;i<=array2d[0].length-1;i++)
+			{
+				System.out.print("At index "+i+","+j+": "+array2d[j][i]+" ");
+			}
+			System.out.println();
+		}
+		String [][]array2dstr =new String[][]{{"Ravinder","Big Data"},{"Mark","Oracle DBA"},{"John",".Net"},{"Kapler","Sql"},{"Peter","Front End Developer"}};
+		for(int j=array2dstr.length-1;j>=0;j--)
+		{
+			for(int i=0;i<=array2dstr[0].length-1;i++)
+			{
+				System.out.print("At index "+i+","+j+": "+array2dstr[j][i]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
