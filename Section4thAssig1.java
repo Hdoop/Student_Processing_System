2e045aadf2d9390f00193a80e920965f0d75194b
ravinder singh <ravinder@atjam.com>
2015-12-21 20:49:59 +0530

    Print initialized array in reverse order
    end

--- Section4thAssig1.java
+++ Section4thAssig1.java
@@ -23,11 +23,35 @@ public class Section4thAssig1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Print the initalized array in reverse order
 		int []arrayx =new int[]{10,20,30,40,50};
 		for(int i=arrayx.length-1;i>=0;i--)
 		{
 			System.out.println("At index "+i+": "+arrayx[i]);
 		}
+		
+		
+		//Print 2d array in reverse order 
+		int [][]array2d =new int[][]{{10,20},{20,30},{30,40},{40,50},{50,60}};
+		for(int j=array2d.length-1;j>=0;j--)
+		{
+			for(int i=0;i<=array2d[0].length-1;i++)
+			{
+				System.out.print("At index "+i+","+j+": "+array2d[j][i]+" ");
+			}
+			System.out.println();
+		}
+		//For string data type .
+		String [][]array2dstr =new String[][]{{"Ravinder","Big Data"},{"Mark","Oracle DBA"},{"John",".Net"},{"Kapler","Sql"},{"Peter","Front End Developer"}};
+		for(int j=array2dstr.length-1;j>=0;j--)
+		{
+			for(int i=0;i<=array2dstr[0].length-1;i++)
+			{
+				System.out.print("At index "+i+","+j+": "+array2dstr[j][i]+" ");
+			}
+			System.out.println();
+		}
+		//By taking input from use and print it in reverse order 
 		int []arrayinput =new int[5];
 		System.out.println("Enter five values :");
 		Scanner sc = new Scanner(System.in);
@@ -40,26 +64,6 @@ public class Section4thAssig1 {
 		for(int i=arrayinput.length-1;i>=0;i--)
 		{
 			System.out.println("At index "+i+": "+arrayinput[i]);
-		}
-		
-		
-		int [][]array2d =new int[][]{{10,20},{20,30},{30,40},{40,50},{50,60}};
-		for(int j=array2d.length-1;j>=0;j--)
-		{
-			for(int i=0;i<=array2d[0].length-1;i++)
-			{
-				System.out.print("At index "+i+","+j+": "+array2d[j][i]+" ");
-			}
-			System.out.println();
-		}
-		String [][]array2dstr =new String[][]{{"Ravinder","Big Data"},{"Mark","Oracle DBA"},{"John",".Net"},{"Kapler","Sql"},{"Peter","Front End Developer"}};
-		for(int j=array2dstr.length-1;j>=0;j--)
-		{
-			for(int i=0;i<=array2dstr[0].length-1;i++)
-			{
-				System.out.print("At index "+i+","+j+": "+array2dstr[j][i]+" ");
-			}
-			System.out.println();
 		}
 	}
 }
